3dff5feb1a615f75b311de402240dc46f3b6c81b
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2021-10-27 22:58:27 +0100

    ES6 Pagination (John Smigla Project): Adding Buttons Functionality ...

--- 1/app.js
+++ 1/app.js
@@ -24,4 +24,25 @@ const init = async () => {
     setupUI()
 }
 
+btnContainer.addEventListener('click', function (event) {
+    if(event.target.classList.contains('btn-container')) return
+    if(event.target.classList.contains('page-btn')) {
+        console.log(parseInt(event.target.dataset.index))
+        index = parseInt(event.target.dataset.index)
+    }
+    if(event.target.classList.contains('next-btn')) {
+        index++
+        if(index > pages.length - 1) {
+            index = 0
+        }
+    }
+    if(event.target.classList.contains('prev-btn')) {
+        index--
+        if(index < 0) {
+            index = pages.length - 1
+        }
+    }
+    setupUI()
+})
+
 window.addEventListener('load', init)
